d3f5b57ee4a8b1f8b6b6fa210096c7eb04be66a0
ceo92 <king9243@naver.com>
2024-09-30 23:26:19 +0900

    refactor: Stock 도메인 리팩토링(편의 메서드 추가 및 @DateTimeFormat 형식 변환)

--- src/main/java/allliveyoung/wms/domain/Stock.java
+++ src/main/java/allliveyoung/wms/domain/Stock.java
@@ -1,5 +1,6 @@
 package allliveyoung.wms.domain;
 
+
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,8 +9,7 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
-@Getter
-@Setter
+@Getter @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
@@ -18,10 +18,11 @@ public class Stock {
   private Long id;
   private String stockCode;
 
-  @DateTimeFormat(pattern =  "yyyy-MM-dd hh:mm:ss")
+  @DateTimeFormat(pattern = "yyyy. MM. dd. a hh:mm")
   private LocalDateTime regDate;
 
-  @DateTimeFormat(pattern =  "yyyy-MM-dd hh:mm:ss")
+
+  @DateTimeFormat(pattern = "yyyy. MM. dd. a hh:mm")
   private LocalDateTime modDate;
 
   private Product product; // 상품명 , 회사명 , 유효기간 , 제조번호 , 보관온도 , 상품 종류 , 총 박스 수량 전부 Product 혹은 Product의 연관관계로 알 수 있음!
@@ -29,5 +30,14 @@ public class Stock {
   private Warehouse warehouse; //창고명 , 창고주소
 
   private Pallet pallet;
+
+  public Integer getTotalStockCount(){
+    int palletQuantity = pallet.getInboundRequestProduct().getPalletQuantity();
+    int boxQuantity = pallet.getInboundRequestProduct().getBoxQuantity();
+    return palletQuantity * boxQuantity;
+  }
+
+
+
 
 }
