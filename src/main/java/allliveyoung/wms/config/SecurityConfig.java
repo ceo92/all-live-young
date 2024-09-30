d66dc0b9b8bbce352c0a9654e4e8334143d16786
devyumi <yyumiwing@naver.com>
2024-09-30 17:23:36 +0900

    feat: SecurityConfig 설정
    requestMatchers, PasswordEncoder, login, logout 추가

---
+++ src/main/java/allliveyoung/wms/config/SecurityConfig.java
@@ -0,0 +1,65 @@
+package allliveyoung.wms.config;
+
+import allliveyoung.wms.config.filter.LoginFail;
+import allliveyoung.wms.config.filter.LoginSuccess;
+import allliveyoung.wms.config.filter.LogoutSuccess;
+import allliveyoung.wms.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Component
+public class SecurityConfig {
+    private final UserDetailsServiceImpl userDetailsService;
+    private final LoginSuccess loginSuccess;
+    private final LoginFail loginFail;
+    private final LogoutSuccess logoutSuccess;
+    private final String[] admin = {"/members/requests/**", "/members/cancel/**", "/warehouses/save/*", "/warehouses/*/update-search"};
+    private final String[] manager = {"/inbound-requests/*/update-status", "/expenses/save", "/expenses/*/update", "/sales/save", "/sales/*/update", "/warehouses/save", "/warehouses/*/update-request"};
+    private final String[] adminAndManager = {"/members", "/announcements/save", "/announcements/*/**", "/inquiries/*/**", "/answers", "/sales", "/expenses/statistic", "/stocks/*/**"};
+    private final String[] company = {"/inbound-requests/save", "/inbound-requests/*/update", "/inbound-requests/*/delete", "/outbound-requests/save", "/inquiries/save", "/inquiries/*/update"};
+    private final String[] all = {"/", "/inbound-requests", "/outbound-requests", "/members/*/**", "/announcements", "/inquiries", "/inquiries/*", "/expenses", "/stocks", "/stocks/*", "/warehouses", "/warehouses/*"};
+    private final String[] permitAll = {"/join", "/find-account", "/reset-password", "/join-type"};
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requestMatcher -> requestMatcher
+                        .requestMatchers(adminAndManager).hasAnyRole("ADMIN", "MANAGER")
+                        .requestMatchers(admin).hasRole("ADMIN")
+                        .requestMatchers(manager).hasRole("MANAGER")
+                        .requestMatchers(company).hasRole("COMPANY")
+                        .requestMatchers(all).authenticated()
+                        .requestMatchers(permitAll).permitAll()
+                        .anyRequest().authenticated())
+
+                .formLogin(login -> login
+                        .loginPage("/login")
+                        .loginProcessingUrl("/login")
+                        .defaultSuccessUrl("/")
+                        .successHandler(loginSuccess)
+                        .failureHandler(loginFail)
+                        .permitAll())
+
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/")
+                        .logoutSuccessHandler(logoutSuccess)
+                        .deleteCookies("JSESSIONID"))
+
+                .userDetailsService(userDetailsService);
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
