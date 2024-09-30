66fa9d8e2f33aa32c86b9d8204eeb3429b43463e
동우 <ddokkochi@gmail.com>
2024-10-01 02:54:28 +0900

    - in progress : 누락 파일 push

---
+++ src/main/java/allliveyoung/wms/domain/Member.java
@@ -0,0 +1,30 @@
+package allliveyoung.wms.domain;
+
+import allliveyoung.wms.constant.AccountStatus;
+import allliveyoung.wms.constant.RoleType;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Member {
+  private Long memberId;
+  private String name;
+  private String password;
+  private String phoneNumber;
+  private String email;
+  private String roadNameAddress;
+  private String detailsAddress;
+  private AccountStatus accountStatus;
+  private LocalDateTime joinDate;
+  private LocalDateTime lastLoginDate;
+  private RoleType roleType;
+  private String businessNumber;
+  private Long warehouseId;
+  private Boolean isAgree;
+  private LocalDateTime agreeDate;
+}
---
+++ src/main/java/allliveyoung/wms/domain/Product.java
@@ -0,0 +1,21 @@
+package allliveyoung.wms.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+  private Long id;
+  private String name;
+  private Long memberId;
+  private String storeTemperature;
+  private String type;
+  private LocalDateTime licenseDate;
+  private int licenseNum;
+
+}
