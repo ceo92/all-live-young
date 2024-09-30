7c4088fc2a2cfce1dbd84d6ebcec0ab8010fcc8d
devyumi <yyumiwing@naver.com>
2024-09-30 17:24:30 +0900

    feat: login, logout Custom Handler 추가

---
+++ src/main/java/allliveyoung/wms/config/filter/LoginFail.java
@@ -0,0 +1,25 @@
+package allliveyoung.wms.config.filter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class LoginFail extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+        setDefaultFailureUrl("/login?error");
+        log.error("로그인 실패");
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
