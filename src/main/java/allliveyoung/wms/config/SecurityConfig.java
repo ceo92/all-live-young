0b94a2aa73271ca1110f92fa4e51526aaa157ff0
devyumi <yyumiwing@naver.com>
2024-09-30 23:35:56 +0900

    refactor: security 권한 url 재설정

--- src/main/java/allliveyoung/wms/config/SecurityConfig.java
+++ src/main/java/allliveyoung/wms/config/SecurityConfig.java
@@ -23,9 +23,9 @@ public class SecurityConfig {
     private final LogoutSuccess logoutSuccess;
     private final String[] admin = {"/members/requests/**", "/members/cancel/**", "/warehouses/save/*", "/warehouses/*/update-search"};
     private final String[] manager = {"/inbound-requests/*/update-status", "/expenses/save", "/expenses/*/update", "/sales/save", "/sales/*/update", "/warehouses/save", "/warehouses/*/update-request"};
-    private final String[] adminAndManager = {"/members", "/announcements/save", "/announcements/*/**", "/inquiries/*/**", "/answers", "/sales", "/expenses/statistic", "/stocks/*/**"};
+    private final String[] adminAndManager = {"/members", "/announcements/save", "/announcements/*/**", "/inquiries/*/**", "/answers", "/expenses/statistic", "/stocks/*/**"};
     private final String[] company = {"/inbound-requests/save", "/inbound-requests/*/update", "/inbound-requests/*/delete", "/outbound-requests/save", "/inquiries/save", "/inquiries/*/update"};
-    private final String[] all = {"/", "/inbound-requests", "/outbound-requests", "/members/*/**", "/announcements", "/inquiries", "/inquiries/*", "/expenses", "/stocks", "/stocks/*", "/warehouses", "/warehouses/*"};
+    private final String[] all = {"/", "/inbound-requests", "/outbound-requests", "/members/*/**", "/announcements", "/inquiries", "/inquiries/*", "/expenses", "/sales", "/stocks", "/stocks/*", "/warehouses", "/warehouses/*"};
     private final String[] permitAll = {"/join", "/find-account", "/reset-password", "/join-type"};
 
     @Bean
